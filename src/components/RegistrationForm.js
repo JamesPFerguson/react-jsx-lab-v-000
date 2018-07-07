0d468aa3852bbb45e6128b1e6498f4c2a3ff89b5
James Ferguson <ncjamesferguson@gmail.com>
2018-07-07 23:01:00 +0000

    automatically backed up by learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -4,9 +4,9 @@ class RegistrationForm extends React.Component {
   render() {
     return (
       <form>
-      <input type="text"> </input>
-      <input type="password"> </input>
-      <button type="submit">
+      <input type="text" />
+      <input type="password" />
+      <button type="submit" />
     )
   }
 }
